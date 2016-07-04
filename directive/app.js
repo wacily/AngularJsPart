26e68dc551c89e949362d8e39a48c6997b99f9b1
mahui <hui.ma@capitalonline.net>
2016-07-04 17:53:30 +0800

    finsh dircetive

--- directive/app.js
+++ directive/app.js
@@ -92,7 +92,72 @@ app.directive('hello4',function(){
 	}
 });
 
-app.controller("secondController",['$scope',function($scope){
+app.controller("threeController",['$scope',function($scope){
 	$scope.title='点击展开';
 	$scope.text = '这里是内部的内容。';
+}]);
+
+//指令7
+app.directive('accordion',function(){
+	return {
+		restrict:'EA',
+		replace:true,
+		transclude:true,
+		template:'<div ng-transclude></div>',
+		controller:function(){
+			var expanders = [];
+			this.gotOpened = function(selectedExpander){
+				angular.forEach(expanders,function(expander){
+					if(selectedExpander != expander){
+						expander.showMe = false;
+					}
+				});
+			}
+			
+			this.addExpander = function(expander){
+				expanders.push(expander)
+			}
+		}
+	}
+});
+
+app.directive('expander',function(){
+	return {
+		restrict:'EA',
+		replace:true,
+		transclude:true,
+		require:'^?accordion',
+		scope:{
+			title:'=expanderTitle'
+		},
+		template:'<div>'
+						+'<div class="title" ng-click="toggle()">{{title}}</div>'
+						+'<div class="body" ng-show="showMe" ng-transclude></div>'
+						+'</div>',
+		link:function(scope,element,attrs,accordionController){
+			scope.showMe = false;
+			accordionController.addExpander(scope);
+			scope.toggle = function(){
+				scope.showMe = !scope.showMe;
+				accordionController.gotOpened(scope);
+			}
+		}
+	}
+});
+
+app.controller('someController',['$scope',function($scope){
+	$scope.expanders = [
+		{
+			title:'Click me to expand',
+			text:'Hi trere folks,I am the content that was hidden but is now shown.'
+		},
+		{
+			title:'Click this',
+			text:'I am even better text than you have seen previously'
+		},
+		{
+			title:'Test',
+			text:'text'
+		}
+	];
 }]);
